81d7ce704ad75e618f0fb8a927d35e608069bb55
Piotr Jurczyński <piotr.jurczynski@netguru.pl>
2014-02-24 16:56:11 +0100

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Spree::Core::Engine.routes.draw do
   resources :user_product_photos, only: [:create]
+
+  namespace :admin do
+    resources :products do
+      resources :user_photos
+      collection do
+        get :pending_user_photos
+      end
+    end
+  end
 end
