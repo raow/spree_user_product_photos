b594f7aba3979fa520641e16ab2a661b486363cb
Piotr Jurczyński <piotr.jurczynski@netguru.pl>
2014-02-25 13:12:43 +0100

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Spree::Core::Engine.routes.draw do
 
   namespace :admin do
     resources :products do
-      resources :user_product_photos do
+      resources :user_product_photos, only: [] do
         member do
           post :accept
           post :reject
         end
       end
-      resources :user_product_photos
       collection do
         get :pending_user_photos
       end
