cbd7ea25ada0ab5b7d3ed6cddfab83420a0458cf
zzk <zzk@chickenkiller.org>
2014-07-14 14:09:04 +0200

    Use up/down method instead of change

--- db/migrate/20140220084916_create_spree_user_product_photos.rb
+++ db/migrate/20140220084916_create_spree_user_product_photos.rb
@@ -1,5 +1,5 @@
 class CreateSpreeUserProductPhotos < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :spree_user_product_photos do |t|
       t.integer :user_id
       t.integer :product_id
@@ -8,5 +8,10 @@ class CreateSpreeUserProductPhotos < ActiveRecord::Migration
       t.timestamps
     end
     add_attachment :spree_user_product_photos, :file
+  end
+
+  def self.down
+    remove_attachment :spree_user_product_photos, :file
+    drop_table :spree_user_product_photos
   end
 end
