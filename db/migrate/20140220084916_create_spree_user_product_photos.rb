af08e31f662e95345262e8390fa1ff4e459c2a7a
zzk <zzk@chickenkiller.org>
2014-07-14 16:04:42 +0200

    Revert "Use up/down method instead of change"
    This reverts commit cbd7ea25ada0ab5b7d3ed6cddfab83420a0458cf.

--- db/migrate/20140220084916_create_spree_user_product_photos.rb
+++ db/migrate/20140220084916_create_spree_user_product_photos.rb
@@ -1,5 +1,5 @@
 class CreateSpreeUserProductPhotos < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :spree_user_product_photos do |t|
       t.integer :user_id
       t.integer :product_id
@@ -8,10 +8,5 @@ class CreateSpreeUserProductPhotos < ActiveRecord::Migration
       t.timestamps
     end
     add_attachment :spree_user_product_photos, :file
-  end
-
-  def self.down
-    remove_attachment :spree_user_product_photos, :file
-    drop_table :spree_user_product_photos
   end
 end
